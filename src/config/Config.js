797639cd13c628ceea1d4d0a7d8d5d202cedcf0f
KhushbooVarathe <khushboo.va@cisinlabs.com>
2023-08-28 18:38:10 +0530

    mess

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,3 +1,3 @@
-const baseUrl = 'http://localhost:7000'; // Default to local development URL
-// const baseUrl = 'https://backend-eoh8.onrender.com';
+// const baseUrl = 'http://localhost:7000'; // Default to local development URL
+const baseUrl = 'https://finalbackend-n3de.onrender.com';
 export default baseUrl;
